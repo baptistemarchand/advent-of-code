96b67dbccb90c86d472a312d5762183aa47f5111
Baptiste Marchand <baptiste.marchand@ambler.fr>
2020-12-03 22:38:30 +0100

    Cleanup

--- d3.ts
+++ d3.ts
@@ -1,24 +1,23 @@
+// Run with `deno run --allow-read d3.ts`
+
 let input = await Deno.readTextFile("input.txt");
-const map: string[] = []
-for (const line of input.split('\n').filter(Boolean)) {
-    map.push(line)
-}
-const width = map[0].length
-const height = map.length
 
-const get = (x: number, y: number) => {
-    return map[y][x%width]
-}
+const map = input.split("\n");
+const width = map[0].length;
+const height = map.length;
 
-let x = 0
-let y = 0
-let count = 0
+const isTree = (x: number, y: number) => map[y][x % width] === "#";
+
+let x = 0, y = 0, treeCount = 0;
+
 while (y < height) {
-    if (get(x,y) === '#') {count++}
-    x += 1
-    y += 2
+  if (isTree(x, y)) {
+    treeCount++;
+  }
+  x += 1;
+  y += 2;
 }
-console.log(count);
 
+console.log(treeCount);
 
-export {}
+export {}; // To prevent isolatedModules error
