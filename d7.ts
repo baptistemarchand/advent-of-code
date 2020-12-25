27123972f67629e9fd92d3f062c5eb9ba4d49c2a
Baptiste Marchand <baptiste.marchand@ambler.fr>
2020-12-25 20:47:34 +0100

    cleanup

--- d7.ts
+++ d7.ts
@@ -1,26 +1,40 @@
 // https://adventofcode.com/2020/day/7 | Run with `deno run --allow-read d7.ts`
 
-const rGraph: Record<string, string[]> = {} // "reverse" graph
-const graph: Record<string, [string, number][]> = {}
+const rules = (await Deno.readTextFile('input.txt')).split('.\n')
 
-for (const rule of (await Deno.readTextFile('input.txt')).split('.\n').filter(Boolean)) {
+const rGraph: Record<string, string[]> = {} // "reverse" graph
+
+for (const rule of rules) {
   const [name, content] = rule.split(' bags contain ')
-  rGraph[name] = rGraph[name] ?? []
-  graph[name] = graph[name] ?? []
-  for (const bag of content === 'no other bags' ? [] : content.split(', ')) {
-    const [_, count, color] = bag.match(/(\d+) ([a-z]+ [a-z]+) bags?/)!
-    ;(graph[name] = graph[name] ?? []).push([color, Number(count)])
-    ;(rGraph[color] = rGraph[color] ?? []).push(name)
+  if (!rGraph[name]) {
+    rGraph[name] = []
+  }
+  if (content === 'no other bags') {
+    continue
+  }
+
+  for (const bag of content.split(', ')) {
+    const [_, color] = bag.match(/\d+ ([a-z]+ [a-z]+) bags?/)!
+    if (!rGraph[color]) {
+      rGraph[color] = []
+    }
+    rGraph[color].push(name)
   }
 }
 
-// part 1
-const go = (color = 'shiny gold'): string[] => rGraph[color].reduce((result, next) => result.concat(go(next)), [color])
-console.log(new Set(go()).size - 1) // Remove one because "shiny gold" is included in the list
+console.log(rGraph)
 
-// part 2
-const go2 = (color = 'shiny gold'): number => graph[color].reduce((total, [next, n]) => total + n * go2(next), 1)
+const getReachableColors = (color: string): string[] => {
+  let result = [color]
+  const nextColors = rGraph[color]
+  for (const nextColor of nextColors) {
+    const reachableColors = getReachableColors(nextColor)
+    result = result.concat(reachableColors)
+  }
 
-console.log(go2() - 1)
+  return result
+}
+
+console.log(getReachableColors('shiny gold'))
 
 export {} // To prevent isolatedModules error
