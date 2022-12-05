871c9745d74c7b7ed517aac7a0c2ccb4b5e5f661
Baptiste Marchand <baptiste.marchand@ambler.fr>
2022-12-05 17:41:00 +0100

    wip

--- 2022/d4.ts
+++ 2022/d4.ts
@@ -3,7 +3,7 @@
 type Range = [number, number]
 
 const contains = ([minA, maxA]: Range, [minB, maxB]: Range) => minA <= minB && maxA >= maxB
-const overlaps = ([minA, maxA]: Range, [minB, maxB]: Range) => !(maxA < minB || maxB < minA)
+const overlaps = ([minA, maxA]: Range, [minB, maxB]: Range) => maxA >= minB && maxB >= minA
 
 const result = (await Deno.readTextFile('./input.txt')).split('\n').filter(line => {
   const [a, b] = line.split(',').map(r => r.split('-').map(Number) as Range)
