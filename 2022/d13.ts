b489bc8a102d061d9135e186e4729fd72446dfbd
Baptiste Marchand <baptiste.marchand@ambler.fr>
2022-12-13 22:01:13 +0100

    d13 cleanup

--- 2022/d13.ts
+++ 2022/d13.ts
@@ -1,66 +1,23 @@
 // https://adventofcode.com/2022/day/13 | Run with `deno run --allow-read d13.ts`
 
-const pairs = (await Deno.readTextFile('./input.txt')).split('\n\n').map(pair => pair.split('\n'))
+const pairs = (await Deno.readTextFile('./input.txt')).split('\n\n').map(pair => pair.split('\n').map(eval))
 
-const parseList = (s: string): string[] => {
-  if (!s.includes('[')) {
-    s = `[${s}]`
-  }
-
-  if (s === '[]') {
-    return []
-  }
-
-  const result: string[] = []
-  let h = 0
-  let curr = ''
-
-  for (let i = 1; i < s.length; i++) {
-    if (h === 0) {
-      if (s[i] === ',' || s[i] === ']') {
-        result.push(curr)
-        curr = ''
-      } else if (s[i] === '[') {
-        curr += '['
-        h++
-      } else {
-        curr += s[i]
-      }
-    } else {
-      if (s[i] === '[') {
-        h++
-      } else if (s[i] === ']') {
-        h--
-      }
-      curr += s[i]
-    }
-    s[i]
-  }
-
-  return result
-}
-
-const cmp = (a: string, b: string): 'OK' | 'EQUAL' | 'NOK' => {
-  if (a === b) {
-    return 'EQUAL'
-  }
+const cmp = (a: any, b: any): 'OK' | 'EQUAL' | 'NOK' => {
   if (a == undefined) {
     return 'OK'
   }
   if (b == undefined) {
     return 'NOK'
   }
-  if (!a.includes('[') && !b.includes('[')) {
-    if (+a < +b) {
-      return 'OK'
+  if (typeof a === 'number' && typeof b === 'number') {
+    if (a === b) {
+      return 'EQUAL'
     }
-    if (+a > +b) {
-      return 'NOK'
-    }
-    return 'EQUAL'
+    return a < b ? 'OK' : 'NOK'
   }
-  let la = parseList(a)
-  let lb = parseList(b)
+
+  let la = typeof a === 'number' ? [a] : a
+  let lb = typeof b === 'number' ? [b] : b
 
   while (la.length || lb.length) {
     const c = cmp(la[0], lb[0])
@@ -85,7 +42,8 @@ for (let i = 0; i < pairs.length; i++) {
 
 console.log(total) // part 1
 
-const lines = (await Deno.readTextFile('./input.txt')).split('\n').filter(Boolean)
-lines.push('[[2]]', '[[6]]')
+const lines = (await Deno.readTextFile('./input.txt')).split('\n').filter(Boolean).map(eval)
+lines.push([[2]], [[6]])
 lines.sort((a, b) => (cmp(a, b) === 'OK' ? -1 : 1))
-console.log((lines.indexOf('[[2]]') + 1) * (lines.indexOf('[[6]]') + 1))
+const linesAsText = lines.map(l => JSON.stringify(l))
+console.log((linesAsText.indexOf('[[2]]') + 1) * (linesAsText.indexOf('[[6]]') + 1)) // part 2
