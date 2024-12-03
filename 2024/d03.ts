12f3e3e008382f6bd14b185ebafd73d9fc0c4242
Baptiste Marchand <baptiste.marchand@amblea.fr>
2024-12-03 08:46:05 +0100

    2024/d03

---
+++ 2024/d03.ts
@@ -0,0 +1,25 @@
+// https://adventofcode.com/2024/day/3 | Run with `deno run --allow-read d03.ts`
+
+import {sumOf} from '@std/collections'
+
+const input = await Deno.readTextFile('./input.txt')
+
+const getTotal = (line: string) => sumOf([...line.matchAll(/mul\((\d+),(\d+)\)/g)], m => +m[1] * +m[2])
+
+const part1 = getTotal(input)
+console.log(part1)
+
+let part2 = 0
+let enabled = true
+
+for (const chunk of input.split(/(don't\(\)|do\(\))/)) {
+  if (chunk === "don't()") {
+    enabled = false
+  } else if (chunk === 'do()') {
+    enabled = true
+  } else if (enabled) {
+    part2 += getTotal(chunk)
+  }
+}
+
+console.log(part2)
