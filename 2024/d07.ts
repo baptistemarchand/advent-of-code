2a7cb0920f25cfd8bce4c0292510498b70c1a534
Baptiste Marchand <baptiste.marchand@amblea.fr>
2024-12-07 11:13:03 +0100

    2024/d07

---
+++ 2024/d07.ts
@@ -0,0 +1,24 @@
+import {sumOf} from '@std/collections'
+import {getNumbers} from '../utils.ts'
+
+const lines = (await Deno.readTextFile('./input.txt')).split('\n').map(getNumbers)
+
+const solve = (ops: Array<(a: number, b: number) => number>) => {
+  const getResults = (ns: number[]): number[] => {
+    if (ns.length === 1) {
+      return [ns[0]]
+    }
+
+    return getResults(ns.slice(1)).flatMap(x => ops.map(op => op(x, ns[0])))
+  }
+
+  return sumOf(
+    lines.filter(([total, ...parts]) => getResults(parts.reverse()).some(p => p === total)),
+    ([total]) => total,
+  )
+}
+
+const part1 = solve([(a, b) => a + b, (a, b) => a * b])
+const part2 = solve([(a, b) => a + b, (a, b) => a * b, (a, b) => +`${a}${b}`])
+
+console.log(part1, part2)
