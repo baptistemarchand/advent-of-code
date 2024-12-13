1e19bc185074ddae982da338b7acdb7b3d9840ce
Baptiste Marchand <baptiste.marchand@amblea.fr>
2024-12-13 15:23:51 +0100

    cleanup

--- 2024/d13.ts
+++ 2024/d13.ts
@@ -1,6 +1,8 @@
-import {getNumbers, solveTwoEquations} from '../utils.ts'
+import {getNumbers, Point, solveTwoEquations} from '../utils.ts'
 
-const machines = (await Deno.readTextFile('./input.txt')).split('\n\n').map(m => {
+type Machine = {a: Point; b: Point; prize: Point}
+
+const machines: Machine[] = (await Deno.readTextFile('./input.txt')).split('\n\n').map(m => {
   const [a, b, prize] = m.split('\n').map(getNumbers)
   return {
     a: {x: a[0], y: a[1]},
@@ -9,8 +11,12 @@ const machines = (await Deno.readTextFile('./input.txt')).split('\n\n').map(m =>
   }
 })
 
-const getTokens = ({prize, a, b}: (typeof machines)[0]) => {
-  const res = solveTwoEquations({a: a.x, b: b.x, r: prize.x}, {a: a.y, b: b.y, r: prize.y})
+const getTokens = ({prize, a, b}: Machine) => {
+  const res = solveTwoEquations(
+    // We want to find the A and B pair that resolves the equations :
+    {a: a.x, b: b.x, r: prize.x}, // Eqation for X : (a.x * A) + (b.x * B) = prize.x
+    {a: a.y, b: b.y, r: prize.y}, // Eqation for Y : (a.y * A) + (b.y * B) = prize.y
+  )
 
   if (!res || res.x % 1 !== 0 || res.y % 1 !== 0) {
     return 0
@@ -20,6 +26,7 @@ const getTokens = ({prize, a, b}: (typeof machines)[0]) => {
 }
 
 let [part1, part2] = [0, 0]
+
 for (const machine of machines) {
   part1 += getTokens(machine)
   machine.prize.x += 10000000000000
