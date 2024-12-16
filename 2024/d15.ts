44142e805bee9e27d26e5a6876f5b612d0c01217
Baptiste Marchand <baptiste.marchand@amblea.fr>
2024-12-16 14:23:04 +0100

    wip

--- 2024/d15.ts
+++ 2024/d15.ts
@@ -1,13 +1,13 @@
 import chalk from 'jsr:@nothing628/chalk'
-import {Point, printGrid, walkGrid} from '../utils.ts'
+import {Point, walkGrid} from '../utils.ts'
 import {sumOf} from '@std/collections'
 
 const [map_, moves_] = (await Deno.readTextFile('./input.txt')).split('\n\n')
 const moves = moves_.replaceAll('\n', '').split('')
 
-type Box = {id?: number; x1?: number; x2?: number; x?: number; y: number}
-const boxes1: Box[] = []
-const boxes2: Box[] = []
+type Group = {id?: number; points: Point[]}
+const boxes1: Group[] = []
+const boxes2: Group[] = []
 const robot1: Point = {x: 0, y: 0}
 const robot2: Point = {x: 0, y: 0}
 
@@ -23,7 +23,7 @@ walkGrid(map1, ({e, x, y, p}) => {
     map2[y].push('#', '#')
   } else if (e === 'O') {
     map2[y].push('[', ']')
-    boxes1.push(p)
+    boxes1.push({points: [p]})
   } else if (e === '.') {
     map2[y].push('.', '.')
   }
@@ -35,7 +35,13 @@ walkGrid(map2, ({e, x, y}) => {
     robot2.x = x
     robot2.y = y
   } else if (e === '[') {
-    boxes2.push({id: id++, x1: x, x2: x + 1, y})
+    boxes2.push({
+      id: id++,
+      points: [
+        {x, y},
+        {x: x + 1, y},
+      ],
+    })
   }
 })
 
@@ -46,60 +52,57 @@ const a: Record<string, Point> = {
   v: {x: 0, y: 1},
 }
 
-const print = () => {
-  let out = ''
-  for (let y = 0; y < map1.length; y++) {
-    for (let x = 0; x < map1[0].length; x++) {
-      if (map1[y][x] === '#') {
-        out += '#'
-      } else if (robot1.x === x && robot1.y === y) {
-        out += chalk.bgRed('@')
-      } else if (boxes1.some(b => b.x === x && b.y === y)) {
-        out += chalk.bgGreen('O')
-      } else {
-        out += '.'
-      }
-    }
-    out += '\n'
-  }
-  console.log(out)
-}
+// const print = () => {
+//   let out = ''
+//   for (let y = 0; y < map1.length; y++) {
+//     for (let x = 0; x < map1[0].length; x++) {
+//       if (map1[y][x] === '#') {
+//         out += '#'
+//       } else if (robot1.x === x && robot1.y === y) {
+//         out += chalk.bgRed('@')
+//       } else if (boxes1.some(b => b.x === x && b.y === y)) {
+//         out += chalk.bgGreen('O')
+//       } else {
+//         out += '.'
+//       }
+//     }
+//     out += '\n'
+//   }
+//   console.log(out)
+// }
 
 // Returns all the new points that we will touch if we move the object `o` in the direction `dir`
-const getNextPoints = (o: {x?: number; x1?: number; x2?: number; y: number}, dir: Point): Point[] => {
-  // p is a robot or a simple box
-  if (o.x) {
-    return [{x: o.x + dir.x, y: o.y + dir.y}]
-  }
+const getNextPoints = (g: Group, dir: Point): Point[] => {
+  return g.points.flatMap(p => ({x: p.x + dir.x, y: p.y + dir.y}))
 
-  // p is a wide box
-  if (dir.x === 1) {
-    return [{x: o.x2! + 1, y: o.y}]
-  }
-  if (dir.x === -1) {
-    return [{x: o.x1! - 1, y: o.y}]
-  }
+  // // p is a robot or a simple box
+  // if (o.x) {
+  //   return [{x: o.x + dir.x, y: o.y + dir.y}]
+  // }
 
-  return [
-    {x: o.x1!, y: o.y + dir.y},
-    {x: o.x2!, y: o.y + dir.y},
-  ]
+  // // p is a wide box
+  // if (dir.x === 1) {
+  //   return [{x: o.x2! + 1, y: o.y}]
+  // }
+  // if (dir.x === -1) {
+  //   return [{x: o.x1! - 1, y: o.y}]
+  // }
+
+  // return [
+  //   {x: o.x1!, y: o.y + dir.y},
+  //   {x: o.x2!, y: o.y + dir.y},
+  // ]
 }
 
-const canMoveObject = (
-  p: {x?: number; x1?: number; x2?: number; y: number},
-  dir: Point,
-  boxes: Box[],
-  map: typeof map1,
-): boolean => {
-  const nps = getNextPoints(p, dir)
+const canMoveObject = (g: Group, dir: Point, boxes: Group[], map: typeof map1): boolean => {
+  const nps = getNextPoints(g, dir)
 
   if (nps.some(np => map[np.y][np.x] === '#')) {
     return false
   }
 
   const blockingBoxes = nps
-    .map(np => boxes.find(b => b.y === np.y && (b.x === np.x || b.x1 === np.x || b.x2 === np.x)))
+    .map(np => boxes.find(b => b.id !== g.id && b.points.some(p => p.x === np.x && p.y === np.y)))
     .filter(Boolean)
 
   for (const box of blockingBoxes) {
@@ -111,12 +114,12 @@ const canMoveObject = (
   return true
 }
 
-const moveObject = (p: {x?: number; x1?: number; x2?: number; y: number}, dir: Point, boxes: Box[]): boolean => {
-  const nps = getNextPoints(p, dir)
+const moveObject = (g: Group, dir: Point, boxes: Group[]): boolean => {
+  const nps = getNextPoints(g, dir)
 
   const blockingBoxes = nps
-    .map(np => boxes.find(b => b.y === np.y && (b.x === np.x || b.x1 === np.x || b.x2 === np.x)))
-    .filter(Boolean) as Box[]
+    .map(np => boxes.find(b => b.id !== g.id && b.points.some(p => p.x === np.x && p.y === np.y)))
+    .filter(Boolean) as Group[]
 
   const seen = new Set<number>()
   for (const box of blockingBoxes) {
@@ -127,29 +130,22 @@ const moveObject = (p: {x?: number; x1?: number; x2?: number; y: number}, dir: P
     moveObject(box, dir, boxes)
   }
 
-  p.y += dir.y
-  if (p.x) {
+  for (const p of g.points) {
+    p.y += dir.y
     p.x += dir.x
-  } else {
-    p.x1! += dir.x
-    p.x2! += dir.x
   }
+
   return true
 }
 
 for (const move of moves) {
-  // console.log(move)
-
-  if (canMoveObject(robot1, a[move], boxes1, map1)) {
-    moveObject(robot1, a[move], boxes1)
-  } else {
-    // console.log('ILLEGAL MOVe')
+  if (canMoveObject({points: [robot1]}, a[move], boxes1, map1)) {
+    moveObject({points: [robot1]}, a[move], boxes1)
   }
-  // print()
-  if (canMoveObject(robot2, a[move], boxes2, map2)) {
-    moveObject(robot2, a[move], boxes2)
+  if (canMoveObject({points: [robot2]}, a[move], boxes2, map2)) {
+    moveObject({points: [robot2]}, a[move], boxes2)
   }
 }
 
-console.log(sumOf(boxes1, box => box.x! + box.y * 100))
-console.log(sumOf(boxes2, box => box.x1! + box.y * 100))
+console.log(sumOf(boxes1, box => box.points[0].x! + box.points[0].y * 100))
+console.log(sumOf(boxes2, box => box.points[0].x! + box.points[0].y * 100))
