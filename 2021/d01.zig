4859a777e48ef5d8c3194df3ee6e715c5659715e
Baptiste Marchand <baptiste.marchand@amblea.fr>
2026-08-11 00:15:59 +0200

    2026/d01 part 1 with SIMD

--- 2021/d01.zig
+++ 2021/d01.zig
@@ -19,24 +19,43 @@ pub fn main() !void {
 
     var lines = std.mem.tokenizeScalar(u8, bytes, '\n');
 
-    var list: std.ArrayList(u16) = .empty;
+    const T = u16;
+
+    var list: std.ArrayList(T) = .empty;
     defer list.deinit(allocator);
 
     while (lines.next()) |line| {
-        const n = try std.fmt.parseInt(u16, line, 10);
+        const n = try std.fmt.parseInt(T, line, 10);
         try list.append(allocator, n);
     }
 
     const ns = try list.toOwnedSlice(allocator);
     defer allocator.free(ns);
 
-    var total: u32 = 0;
-    var x: u32 = 100000;
+    const lanes = std.simd.suggestVectorLength(T).?;
+    std.debug.print("Using {} lanes\n", .{lanes});
 
+    const Vec = @Vector(lanes, T);
+
+    var total: u32 = 0;
+    var x: u32 = 100000; // For perf analysis
+
+    // Using SIMD for 8x parallel comparisons
+    // https://mitchellh.com/writing/everyone-should-know-simd
+    // On my mac :
+    // - without SIMD: 0.85s
+    // - with SIMD: 0.3s
     while (x > 0) : (x -= 1) {
-        for (ns, 0..) |n, i| {
-            // std.debug.print("{any} {any}\n", .{ i, n });
-            if (i > 0 and n > ns[i - 1]) {
+        var end: u32 = 0;
+        while (end + lanes < ns.len) : (end += lanes) {
+            const v1: Vec = ns[(end + 1)..][0..lanes].*;
+            const v2: Vec = ns[end..][0..lanes].*;
+            const mask: std.meta.Int(.unsigned, lanes) = @bitCast(v1 > v2);
+            total += @popCount(mask);
+        }
+
+        while (end < ns.len) : (end += 1) {
+            if (ns[end] > ns[end - 1]) {
                 total += 1;
             }
         }
